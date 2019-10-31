0f7d387093bca6b6765e9316c2d874108244f671
Sebastian Landwehr <info@dword-design.de>
2019-10-31 03:51:13 +0100

    move implementation to base

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-#!/usr/bin/env node
+import { base } from '@dword-design/base'
 
-const { babelConfigFilename, base, eslintConfigFilename } = require('@dword-design/base-core')
-const { remove } = require('fs-extra')
-const { spawn } = require('child-process-promise')
-
-base({
-  prepare: async () => {
-    await remove('dist')
-    await spawn('eslint', ['--config', eslintConfigFilename, '--ignore-path', '.gitignore', '.'], { stdio: 'inherit' })
-    await spawn('babel', ['--out-dir', 'dist', '--config-file', babelConfigFilename, 'src'], { stdio: 'inherit' })
-  },
-  start: async () => {
-    const watcher = chokidar.watch('src')
-    watcher.on(
-      'all',
-      debounce(
-        async () => {
-          try {
-            await prepare()
-          } catch (error) {
-            if (error.name !== 'ChildProcessError') {
-              console.log(error)
-            }
-          }
-        },
-        200
-      )
-    )
-  }
-})
+export default base
