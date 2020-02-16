3ea7d880d47466f41b412f4d8b226d38b7091f55
Sebastian Landwehr <info@dword-design.de>
2020-02-16 03:05:53 +0100

    remove workspaces

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import { spawn } from 'child-process-promise'
 import { remove, outputFile, copy, stat } from 'fs-extra'
 import getPackageName from 'get-package-name'
-import { flatMap } from '@dword-design/functions'
-import P from 'path'
-import safeRequire from 'safe-require'
 
 const lint = async () => {
   await outputFile('.eslintrc.json', JSON.stringify({ extends: getPackageName(require.resolve('@dword-design/eslint-config')) }, undefined, 2) + '\n')
-  const workspaces = safeRequire(P.join(process.cwd(), 'package.json'))?.workspaces ?? []
   try {
-    await spawn(
-      'eslint',
-      [
-        '--ext', '.js,.json',
-        '--ignore-path', '.gitignore',
-        ...workspaces |> flatMap(pattern => ['--ignore-pattern', pattern]),
-        '.',
-      ],
-      { capture: ['stdout'] },
-    )
+    await spawn('eslint', ['--ext', '.js,.json', '--ignore-path', '.gitignore', '.'], { capture: ['stdout'] })
   } catch ({ stdout }) {
     throw new Error(stdout)
   }
