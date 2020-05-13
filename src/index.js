98a26757802a9ae17cbc91d0985aeace3931837f
foo <foo@bar.de>
2020-05-13 15:45:46 +0200

    fix: upgrades

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+import { endent } from '@dword-design/functions'
+import { outputFile } from 'fs-extra'
+import getPackageName from 'get-package-name'
 import lint from './lint'
 import dev from './dev'
 import prepublishOnly from './prepublish-only'
 
 export default {
+  allowedMatches: ['src'],
   gitignore: ['/.eslintrc.json'],
+  prepare: () =>
+    outputFile(
+      '.eslintrc.json',
+      endent`
+    {
+      "extends": "${getPackageName(
+        require.resolve('@dword-design/eslint-config')
+      )}"
+    }
+
+  `
+    ),
   test: lint,
   commands: {
     dev,
--- src/lint.js
+++ src/lint.js
@@ -1,20 +1,6 @@
 import execa from 'execa'
-import { endent } from '@dword-design/functions'
-import { outputFile } from 'fs-extra'
-import getPackageName from 'get-package-name'
 
 export default async () => {
-  await outputFile(
-    '.eslintrc.json',
-    endent`
-    {
-      "extends": "${getPackageName(
-        require.resolve('@dword-design/eslint-config')
-      )}"
-    }
-
-  `
-  )
   try {
     await execa.command(
       'eslint --fix --ext .js,.json --ignore-path .gitignore .',
