bf4dd5cefd8bc221f1f46de72b6503cf4b91f5c5
Sebastian Landwehr <info@dword-design.de>
2021-02-17 15:53:32 +0100

    fix: resolve eslint plugins relative

--- src/lint.js
+++ src/lint.js
@@ -1,9 +1,23 @@
 import execa from 'execa'
 
-export default async () => {
+export default async options => {
+  options = {
+    resolvePluginsRelativeTo: require.resolve('@dword-design/eslint-config'),
+    ...options,
+  }
   try {
-    await execa.command(
-      'eslint --fix --ext .js,.json --ignore-path .gitignore .',
+    await execa(
+      'eslint',
+      [
+        '--fix',
+        '--ext',
+        '.js,.json',
+        '--ignore-path',
+        '.gitignore',
+        '--resolve-plugins-relative-to',
+        options.resolvePluginsRelativeTo,
+        '.',
+      ],
       { all: true }
     )
   } catch (error) {
