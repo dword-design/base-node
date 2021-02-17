65c70ebf8401d6a1246a2f66df48bd94d4e08cc6
Sebastian Landwehr <info@dword-design.de>
2021-02-17 16:00:08 +0100

    fix: resolve eslint plugins relative (#46)

--- src/lint.spec.js
+++ src/lint.spec.js
@@ -50,4 +50,39 @@ export default {
         "foo' is assigned a value but never used"
       )
     }),
+  'plugin next to config': () =>
+    withLocalTmpDir(async () => {
+      await outputFiles({
+        node_modules: {
+          '@dword-design/eslint-config': {
+            'index.js': endent`
+              module.exports = {
+                plugins: ['foo'],
+              }
+
+            `,
+            'node_modules/eslint-plugin-foo/index.js': '',
+          },
+          'base-config-self/index.js':
+            "module.exports = require('../../../src')",
+          'eslint-plugin-foo/index.js': 'foo bar',
+        },
+        'package.json': JSON.stringify(
+          {
+            baseConfig: 'self',
+          },
+          undefined,
+          2
+        ),
+        'src/index.js': '',
+      })
+      await execa.command('base prepare')
+      await lint({
+        resolvePluginsRelativeTo: P.join(
+          'node_modules',
+          '@dword-design',
+          'eslint-config'
+        ),
+      })
+    }),
 }
