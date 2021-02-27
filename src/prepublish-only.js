a8bc625746bd9f644ba29c73b60b2fc397dfaffd
Sebastian Landwehr <info@dword-design.de>
2021-02-27 04:52:18 +0100

    feat: ignore snapshot folders

--- src/prepublish-only.js
+++ src/prepublish-only.js
@@ -1,26 +1,27 @@
-import packageName from 'depcheck-package-name'
+import deleteEmpty from 'delete-empty'
 import execa from 'execa'
-import { remove } from 'fs-extra'
+import { copy, remove } from 'fs-extra'
+import micromatch from 'micromatch'
+import P from 'path'
 
 import lint from './lint'
 
 export default async options => {
   await lint()
   await remove('dist')
+  // https://github.com/babel/babel/issues/11394
+  await copy('src', 'dist', {
+    filter: path =>
+      !micromatch.isMatch(path, [
+        '**/*.js',
+        '**/__snapshots__',
+        '**/__image_snapshots__',
+      ]),
+  })
+  await deleteEmpty(P.resolve('dist'))
   await execa(
     'babel',
-    [
-      '--config-file',
-      packageName`@dword-design/babel-config`,
-      '--out-dir',
-      'dist',
-      '--copy-files',
-      '--no-copy-ignored',
-      '--ignore',
-      '**/*.spec.js',
-      '--verbose',
-      'src',
-    ],
+    ['--out-dir', 'dist', '--ignore', '**/*.spec.js', '--verbose', 'src'],
     { stdio: options.log === false ? 'pipe' : 'inherit' }
   )
 }
