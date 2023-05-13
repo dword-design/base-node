5479660e1ba2941ebd4d002922685a731efe3971
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-05-13 04:19:36 +0000

    fix: set main and exports only if index.js exists

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import depcheckParserSass from '@dword-design/depcheck-parser-sass'
+import fs from 'fs-extra'
 import loadPkg from 'load-pkg'
+import P from 'path'
 
 import dev from './dev.js'
 import lint from './lint.js'
@@ -24,13 +26,15 @@ export default config => {
     gitignore: ['/dist'],
     lint,
     npmPublish: true,
-    packageConfig: {
-      main: `dist/${config.cjsFallback ? 'cjs-fallback.cjs' : 'index.js'}`,
-      ...(packageConfig.type === 'module' &&
-        !config.cjsFallback && {
-          exports: './dist/index.js',
-        }),
-    },
+    ...(fs.existsSync(P.join('src', 'index.js')) && {
+      packageConfig: {
+        main: `dist/${config.cjsFallback ? 'cjs-fallback.cjs' : 'index.js'}`,
+        ...(packageConfig.type === 'module' &&
+          !config.cjsFallback && {
+            exports: './dist/index.js',
+          }),
+      },
+    }),
     useJobMatrix: true,
   }
 }
